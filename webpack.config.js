7931012a5b9f6f4b21738d395e5ad157ad095fdf
Dmytro Ilnicki <dmytro@ilnicki.me>
2019-02-07 18:54:35 +0200

    Updated js quotemark rule.

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => ({
-    devtool: "inline-source-map",
+    devtool: env.NODE_ENV === 'production' ? 'inline-source-map' : false,
     entry: {
-        main: "./src/main.ts",
-        worker: "./src/worker.ts",
+        main: './src/main.ts',
+        worker: './src/worker.ts',
     },
     mode: env.NODE_ENV,
     module: {
         rules: [
             {
                 test: /\.json$/,
-                use: "json-loader",
+                use: 'json-loader',
             },
             {
                 test: /\.html$/,
-                use: "html-loader",
+                use: 'html-loader',
             },
             {
                 test: /\.scss$/,
                 use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
                 ],
             },
             {
                 test: /\.ts$/,
-                use: "ts-loader",
+                use: 'ts-loader',
                 exclude: /node_modules/,
             },
             {
                 test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/,
                 use: [
-                    "file-loader",
+                    'file-loader',
                 ],
             },
         ],
     },
     resolve: {
-        extensions: [".ts", ".js"],
+        extensions: ['.ts', '.js'],
     },
     output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
         compress: true,
@@ -52,9 +52,9 @@ module.exports = env => ({
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "src/index.html",
+            template: 'src/index.html',
             excludeChunks: [
-                "worker",
+                'worker',
             ],
         }),
     ],
