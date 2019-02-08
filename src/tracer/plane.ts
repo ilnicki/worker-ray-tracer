d11dc5786fdff9d558ae64ebea3e6f39cba52827
ilnicki <dmytro@ilnicki.me>
2019-02-09 01:04:23 +0200

    Plane code verbocity increased.

--- src/tracer/plane.ts
+++ src/tracer/plane.ts
@@ -2,34 +2,34 @@ import { Body, BodyHandler } from './body';
 import { Intersection } from './intersection';
 import { Ray } from './ray';
 import { SurfaceId } from './surface';
-import { dot } from './vector';
+import { dot, norm } from './vector';
 import { Vector } from './vector';
 
 export interface Plane extends Body {
     handlerId: 'plane';
     surfaceId: SurfaceId;
-    norm: Vector;
+    normal: Vector;
     offset: number;
 }
 
-export const makePlane = (norm: Vector, offset: number, surfaceId: SurfaceId): Plane => ({
+export const makePlane = (normal: Vector, offset: number, surfaceId: SurfaceId): Plane => ({
     handlerId: 'plane',
     surfaceId,
-    norm,
+    normal: norm(normal),
     offset,
 });
 
 export const planeHandler: BodyHandler<Plane> = {
     intersect(ray: Ray, plane: Plane): Intersection {
-        const denom = dot(plane.norm, ray.dir);
+        const denom = dot(plane.normal, ray.dir);
 
         if (denom > 0) {
             return null;
         } else {
-            const dist = (dot(plane.norm, ray.start) + plane.offset) / (-denom);
+            const dist = (dot(plane.normal, ray.start) + plane.offset) / (-denom);
             return { body: plane, ray, dist };
         }
     },
 
-    normal: (_pos: Vector, plane: Plane) => plane.norm,
+    normal: (_pos: Vector, plane: Plane) => plane.normal,
 };
