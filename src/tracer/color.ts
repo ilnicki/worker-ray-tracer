e0109404e3ffd8d647ae94b8fe7b0e16c5e550ee
Dmytro Ilnicki <dmytro@ilnicki.me>
2019-03-12 12:05:02 +0200

    Added color factory.

--- src/tracer/color.ts
+++ src/tracer/color.ts
@@ -4,42 +4,48 @@ export interface Color {
     readonly b: number;
 }
 
-export const white: Color = { r: 1.0, g: 1.0, b: 1.0 };
-export const grey: Color = { r: 0.5, g: 0.5, b: 0.5 };
-export const black: Color = { r: 0.0, g: 0.0, b: 0.0 };
+export const color = (r: number, g: number, b: number): Color => ({
+    r,
+    g,
+    b,
+});
+
+export const white: Color = color(1.0, 1.0, 1.0);
+export const grey: Color = color(0.5, 0.5, 0.5);
+export const black: Color = color(0.0, 0.0, 0.0);
 export const background: Color = black;
 export const defaultColor: Color = black;
 
-export const scale = (k: number, v: Color): Color => ({
-    r: k * v.r,
-    g: k * v.g,
-    b: k * v.b,
-});
+export const scale = (k: number, v: Color): Color => color(
+    k * v.r,
+    k * v.g,
+    k * v.b,
+);
 
-export const plus = (v1: Color, v2: Color): Color => ({
-    r: v1.r + v2.r,
-    g: v1.g + v2.g,
-    b: v1.b + v2.b,
-});
+export const plus = (c1: Color, c2: Color): Color => color(
+    c1.r + c2.r,
+    c1.g + c2.g,
+    c1.b + c2.b,
+);
 
-export const times = (v1: Color, v2: Color): Color => ({
-    r: v1.r * v2.r,
-    g: v1.g * v2.g,
-    b: v1.b * v2.b,
-});
+export const times = (c1: Color, c2: Color): Color => color(
+    c1.r * c2.r,
+    c1.g * c2.g,
+    c1.b * c2.b,
+);
 
-export const toDrawingColor = (c: Color): Color => ({
-    r: Math.floor(Math.min(1, c.r) * 255),
-    g: Math.floor(Math.min(1, c.g) * 255),
-    b: Math.floor(Math.min(1, c.b) * 255),
-});
+export const toDrawingColor = (c: Color): Color => color(
+    Math.floor(Math.min(1, c.r) * 255),
+    Math.floor(Math.min(1, c.g) * 255),
+    Math.floor(Math.min(1, c.b) * 255),
+);
 
 export const toString = (c: Color): string => `rgb(${c.r}, ${c.g}, ${c.b}`;
 
-export const fromNumber = (num: number): Color => ({
-    r: ((num >> 16) & 255) / 255,
-    g: ((num >> 8) & 255) / 255,
-    b: (num & 255) / 255,
-});
+export const fromNumber = (num: number): Color => color(
+    ((num >> 16) & 255) / 255,
+    ((num >> 8) & 255) / 255,
+    (num & 255) / 255,
+);
 
 export const fromHex = (hex: string): Color => fromNumber(parseInt(hex, 16));
